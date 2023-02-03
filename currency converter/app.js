78086cd16befcca87d596f43f84de5d2c218e9d2
olanrewaju24 <119400210+olanrewaju24@users.noreply.github.com>
2023-02-03 20:33:07 +0100

    Add files via upload

---
+++ currency converter/app.js
@@ -0,0 +1,9 @@
+const bnt = document. getElementsbyId("btn");
+
+bnt . addEventListener("click", function()  {
+    e. preventDefault()
+const naira = doument. getElementsbyId("naira");
+const dollar = document. getElementById("dollar");
+
+    dollars.value =Math.round ( naira / 700);
+});
